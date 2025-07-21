cbc2be861194041c8a3386df6003cdd456489578
Sam Gaines <samuelmgaines@gmail.com>
2025-07-21 06:47:45 +0100

    Collect whether film is liked by user

--- api/scraper.py
+++ api/scraper.py
@@ -1,5 +1,6 @@
 # api/scraper.py
 from bs4 import BeautifulSoup
+from datetime import datetime, timezone
 from dotenv import load_dotenv
 import json
 import logging
@@ -32,14 +33,20 @@ def extract_user_review(data, review, film_id, username):
     rating_span = review.select_one('p.poster-viewingdata span.rating')
     stars = rating_span.text.strip() if rating_span else None
     rating = convert_stars_to_number(stars)
+    is_liked = (review.select_one('p.poster-viewingdata span.like') is not None)
     if rating is not None:
         data['users'][username]['reviews'][film_id] = {
-            'rating': rating
+            'rating': rating,
+            'is_liked': is_liked
+        }
+    else:
+        data['users'][username]['watches'][film_id] = {
+            'is_liked': is_liked
         }
 
 # returns True if there is another page
 def scrape_letterboxd_page(data, username, page_num):
-    url = f"https://letterboxd.com/{username}/films/page/{page_num}/"
+    url = f"https://letterboxd.com/{username}/films/by/rated-date/page/{page_num}/"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     reviews = soup.select('.poster-container')
@@ -59,15 +66,16 @@ def scrape_letterboxd_users_data(db,
                                  users_collection_name,
                                  films_collection_name,
                                  usernames=['samuelmgaines', 'embrune', 'devinbaron', 'Martendo24680', 'stephaniebrown2', 'nkilpatrick']):
-    data = {"users": {username: {"reviews": {}} for username in usernames}, "films": {}}
+    data = {"users": {username: {"reviews": {}, "watches": {}} for username in usernames}, "films": {}}
 
     for i, username in enumerate(usernames):
         page_num = 1
+        data['users'][username]['last_update_time'] = datetime.now(timezone.utc)
         while page_num > 0:
             logging.info(f"Scraping {username} page {page_num}...")
             time.sleep(15)  # Sleep to avoid hitting the server too hard
-            next_page = scrape_letterboxd_page(data, username, page_num)
-            if next_page: page_num += 1
+            has_next_page = scrape_letterboxd_page(data, username, page_num)
+            if has_next_page: page_num += 1
             else: break
     
     # Upload to MongoDB
@@ -77,7 +85,12 @@ def scrape_letterboxd_users_data(db,
     for username, user_data in data['users'].items():
         users_collection.replace_one(
             {"username": username},  # Match condition
-            {"username": username, "reviews": user_data['reviews']},  # Replacement document
+            {
+                "username": username,
+                "reviews": user_data['reviews'],
+                "watches": user_data.get('watches', {}),
+                "last_update_time": user_data['last_update_time']
+            }, 
             upsert=True
         )
     for film_id, film_data in data['films'].items():
@@ -138,7 +151,7 @@ def main():
     logging.info("Connected to MongoDB")
 
     scrape_letterboxd_users_data(db, users_collection_name, films_collection_name)
-    
+
     # compute_film_stats()
     # logging.info("Stats computed and saved to api/data/film_stats.json")
 
